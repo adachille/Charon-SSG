9a5745edb3549bd40756d326ef6211931fae6667
Anthony D'Achille <adachille3@gatech.edu>
2018-12-22 09:18:12 -0500

    Fix https redirect

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-//  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+if (process.env != null && process.env.PROD_MODE) {
+  app.use('/', require('redirect-https')({
+    body: '<!-- Hello Mr Developer! Please use HTTPS instead -->'
+  }));
+}
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => res.render('pages/index'));
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+   
+module.exports = app;
